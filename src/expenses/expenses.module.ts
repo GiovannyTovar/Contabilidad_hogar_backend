2cf86f811a5ade1638e6ef0f070eb0857b04b3b6
GiovannyTovar <giovannytovar@outlook.com>
2022-04-20 14:28:47 -0500

    Actualizacion service expenses_service. modificar metodo UPDATEEXPENSE modificar modulo

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExpensesUpdateEntity } from 'src/expenses-update/entities/expenses-update.entity';
 import { ExpensesEntity } from './entities/expenses.entity';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExpensesEntity])],
+  imports: [TypeOrmModule.forFeature([ExpensesEntity,ExpensesUpdateEntity])],
   controllers: [ExpensesController],
   providers: [ExpensesService]
 })
